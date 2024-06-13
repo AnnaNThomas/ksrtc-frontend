e21b94f2651bf27ced59f77ccaf4c099244ce8a3
AnnaNThomas <annathomas2003ned@gmail.com>
2024-06-13 23:58:40 +0530

    frontend searchbus completed

---
+++ src/components/SearchBus.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const SearchBus = () => {
+  return (
+    <div>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <div class="text-bg-info p-3">SEARCH BUS</div>
+                <div className="row g-3">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <label htmlFor="" className="form-label">Bus Number</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <buttton className="btn btn-info">Search</buttton>
+                        
+                    </div>
+                   
+                </div>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default SearchBus
