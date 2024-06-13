45221080d193d29f6a3c79b5dde9937b90091ec2
AnnaNThomas <annathomas2003ned@gmail.com>
2024-06-13 23:15:30 +0530

    frontend login created

---
+++ src/components/Login.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <div class="text-bg-info p-3">LOGIN</div>
+                <div className="row g-3">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <label htmlFor="" className="form-label">Email</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <label htmlFor="" className="form-label">Password</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <buttton className="btn btn-info">Login</buttton>
+                        
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <p><a href="#" class="link-info">Goto register</a></p>
+                    </div>
+                </div>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Login
