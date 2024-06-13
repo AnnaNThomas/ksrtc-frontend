f600c2a30adbe984d97326c43ee7739d5dae9039
AnnaNThomas <annathomas2003ned@gmail.com>
2024-06-13 23:28:33 +0530

    frontend Register  created

---
+++ src/components/Register.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <div class="text-bg-info p-3">REGISTER</div>
+                <div className="row g-3">
+                    <div className="col col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">name</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="col col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Email</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="col col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Phoneno</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="col col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Gender</label>
+                        <select name="" id="" class="form-control">
+                            <option class="option">Female</option>
+                            <option class="option">Male</option>
+                        
+                        </select>
+                    </div>
+                    <div className="col col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Password</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="col col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Confirm Password</label>
+                        <input type="text" className="form-control" />
+                    </div>
+
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <buttton className="btn btn-info">Register</buttton>
+                        
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <p><a href="#" class="link-info">Back to login</a></p>
+                    </div>
+                </div>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Register
