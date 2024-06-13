0e6220a9790214ad2b8633a1b64d664ce47dd3de
AnnaNThomas <annathomas2003ned@gmail.com>
2024-06-13 23:39:12 +0530

    frontend AddBus created

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login';
 import Register from './components/Register';
+import AddBus from './components/AddBus';
 
 function App() {
   return (
     <div>
       <Login/>
       <Register/>
+      <AddBus/>
     </div>
   );
 }
